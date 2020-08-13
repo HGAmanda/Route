7947c79cd4e1e4d7b61f0c1f749fe52850a87729
Filipe Silva <filipe.csilvaa@gmail.com>
2020-08-13 19:19:35 -0300

    testing api

--- hughgrace/Api/ApiExample.cs
+++ hughgrace/Api/ApiExample.cs
@@ -7,45 +7,44 @@ using hughgrace.Models;
 
 namespace hughgrace.Api
 {
-    public class ApiExample : IApiEndpoint
+  public class ApiExample : IApiEndpoint
+  {
+    private readonly IAssociateService _associateService;
+    private readonly IRequestParsingService _requestParsing;
+    private readonly IDataService _dataService;
+
+    public ApiExample(IAssociateService associateService, IDataService dataService, IRequestParsingService requestParsing)
     {
-        private readonly IAssociateService _associateService;
-        private readonly IRequestParsingService _requestParsing;
-        private readonly IDataService _dataService;
-
-        public ApiExample(IAssociateService associateService, IDataService dataService, IRequestParsingService requestParsing)
-        {
-            _associateService = associateService;
-            _requestParsing = requestParsing;
-            _dataService = dataService;
-        }
-
-        public ApiDefinition GetDefinition()
-        {
-            return new ApiDefinition
-            {
-                Route = "hughgrace/createTableExample",
-                RequireAuthentication = false
-            };
-        }
-
-        public IApiResponse Post(ApiRequest request)
-        {   
-            using (var dbConnection = new SqlConnection(_dataService.ClientConnectionString.ToString()))
-            {
-                dbConnection.CreateTable<RouteRate>();
-
-                var rObject = _requestParsing.ParseBody<ExampleRequest>(request);
-                var aName = _associateService.GetAssociate(rObject.BackOfficeId).Name;
-
-                return new Ok(new { Status = 1, RequestMessage = rObject.Message, AssociateName = aName });
-            }
-        }
+      _associateService = associateService;
+      _requestParsing = requestParsing;
+      _dataService = dataService;
     }
 
-    public class ExampleRequest
+    public ApiDefinition GetDefinition()
     {
-        public string Message { get; set; }
-        public string BackOfficeId { get; set; }
+      return new ApiDefinition
+      {
+        Route = "hughgrace/createTableExample",
+        RequireAuthentication = false
+      };
     }
+
+    public IApiResponse Post(ApiRequest request)
+    {
+      var rObject = _requestParsing.ParseBody<ExampleRequest>(request);
+      // var aName = _associateService.GetAssociate(rObject.BackOfficeId).Name;
+
+      return new Ok(new { Status = 1, RequestMessage = rObject.Message });
+      //using (var dbConnection = new SqlConnection(_dataService.ClientConnectionString.ToString()))
+      //{
+      //    dbConnection.CreateTable<RouteRate>();
+      //}
+    }
+  }
+
+  public class ExampleRequest
+  {
+    public string Message { get; set; }
+    public string BackOfficeId { get; set; }
+  }
 }
