ef7ce44cbd37efd34a2c26635c5f791aa38b1ca6
Filipe Silva <filipe.csilvaa@gmail.com>
2020-08-14 16:55:36 -0300

    trying to create table v14

--- hughgrace/Api/ApiExample.cs
+++ hughgrace/Api/ApiExample.cs
@@ -9,45 +9,52 @@ using System.Net;
 
 namespace hughgrace.Api
 {
-  public class ApiExample : IApiEndpoint
-  {
-    private readonly IAssociateService _associateService;
-    private readonly IRequestParsingService _requestParsing;
-    private readonly IDataService _dataService;
-
-    public ApiExample(IAssociateService associateService, IDataService dataService, IRequestParsingService requestParsing)
+    public class ApiExample : IApiEndpoint
     {
-      _associateService = associateService;
-      _requestParsing = requestParsing;
-      _dataService = dataService;
-    }
+        private readonly IAssociateService _associateService;
+        private readonly IRequestParsingService _requestParsing;
+        private readonly IDataService _dataService;
 
-    public ApiDefinition GetDefinition()
-    {
-      return new ApiDefinition
-      {
-        Route = "hughgraceServices/createTableExample",
-        RequireAuthentication = false
-      };
-    }
+        public ApiExample(IAssociateService associateService, IDataService dataService, IRequestParsingService requestParsing)
+        {
+            _associateService = associateService;
+            _requestParsing = requestParsing;
+            _dataService = dataService;
+        }
 
-    public IApiResponse Post(ApiRequest request)
-    {
+        public ApiDefinition GetDefinition()
+        {
+            return new ApiDefinition
+            {
+                Route = "hughgraceServices/createTableExample",
+                RequireAuthentication = false
+            };
+        }
+
+        public IApiResponse Post(ApiRequest request)
+        {
+            using (var dbConnection = new SqlConnection(_dataService.ClientConnectionString.ToString()))
+            {
+                dbConnection.CreateTable<RouteRate>();
+            }
             //var rObject = _requestParsing.ParseBody<ExampleRequest>(request);
             //var aName = _associateService.GetAssociate(rObject.BackOfficeId).Name;
 
-        var response = new ApiResponse { StatusCode = HttpStatusCode.OK };
-        return response;
-      //using (var dbConnection = new SqlConnection(_dataService.ClientConnectionString.ToString()))
-      //{
-      //    dbConnection.CreateTable<RouteRate>();
-      //}
+            var response = new ExampleResponse { Status = 1, RequestMessage = "Route" };
+            return new Ok(response);
+        }
     }
-  }
 
-  public class ExampleRequest
-  {
-    public string Message { get; set; }
-    public string BackOfficeId { get; set; }
-  }
+    public class ExampleResponse
+    {
+        public int Status { get; set; }
+        public string RequestMessage { get; set; }
+    }
+
+
+    public class ExampleRequest
+    {
+        public string Message { get; set; }
+        public string BackOfficeId { get; set; }
+    }
 }
