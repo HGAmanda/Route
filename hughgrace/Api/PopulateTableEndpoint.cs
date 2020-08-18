4c1882f8bdee65c8f2c1a3fc112716b89f2d31db
Filipe Silva <filipe.csilvaa@gmail.com>
2020-08-17 22:03:07 -0300

    try to fix transactions v31

--- hughgrace/Api/PopulateTableEndpoint.cs
+++ hughgrace/Api/PopulateTableEndpoint.cs
@@ -29,6 +29,40 @@ namespace hughgrace.Api
             };
         }
 
+        private string CountRouteRateRows()
+        {
+            return "SELECT COUNT(RecordNumber) FROM RouteRate";
+        }
+
+        private string InsertRouteRateTable()
+        {
+            return "INSERT INTO RouteRate (Rate, MinimumChargeAmount, RecordNumber) VALUES ";
+        }
+
+        private string CreateTempTable()
+        {
+            return @"CREATE TABLE #tempTable (
+	            Rate decimal NOT NULL,
+	            MinimumChargeAmount decimal NOT NULL,
+	            RecordNumber int NOT NULL PRIMARY KEY
+            )
+
+            INSERT INTO #tempTable (Rate, MinimumChargeAmount, RecordNumber) VALUES ";
+        }
+
+        private string UpdateRouteRateTable()
+        {
+            return @"UPDATE
+                T1
+            SET
+                [Rate] = T2.[Rate]
+                [MinimumChargeAmount] = T2.[MinimumChargeAmount]
+            FROM
+                RouteRate T1
+            JOIN
+                #tempTable T2 ON T2.[RecordNumber] = T1.[RecordNumber]";
+        }
+
         public IApiResponse Post(ApiRequest request)
         {
             var affect = 0;
@@ -40,67 +74,50 @@ namespace hughgrace.Api
                 return new Ok();
             }
 
-            using (var conn = new SqlConnection(_dataService.ClientConnectionString.ToString()))
+            using (var connection = new SqlConnection(_dataService.ClientConnectionString.ToString()))
             {
-                conn.Open();
-                using (var trans = conn.BeginTransaction(System.Data.IsolationLevel.ReadUncommitted))
+                connection.Open();
+
+                var count = connection.QueryFirstOrDefault<int>(CountRouteRateRows());
+
+                var command = connection.CreateCommand();
+                SqlTransaction transaction;
+
+                transaction = connection.BeginTransaction("RouteTransaction");
+
+                command.Connection = connection;
+                command.Transaction = transaction;
+
+                var sql = new StringBuilder();
+
+                if (count == 0)
                 {
-                    //try
-                    //{
-                        var sql = new StringBuilder();
-                        var count = conn.QueryFirstOrDefault<int>(@"SELECT COUNT(RecordNumber) FROM RouteRate");
-                        if (count == 0)
-                        {
-                            sql.Append(@"INSERT INTO RouteRate (Rate, MinimumChargeAmount, RecordNumber) VALUES ");
-                        }
-                        else
-                        {
-                            sql.Append(@"CREATE TABLE #tempTable (
-	                        Rate decimal NOT NULL,
-	                        MinimumChargeAmount decimal NOT NULL,
-	                        RecordNumber int NOT NULL PRIMARY KEY
-                        )
-
-                        INSERT INTO #tempTable (Rate, MinimumChargeAmount, RecordNumber) VALUES ");
-                        }
-
-                        for (int i = 0; i < req.Rates.Length; i++)
-                        {
-                            var rate = req.Rates[i];
-                            sql.AppendFormat("({0},{1},{2}),", rate.Rate, rate.MinimumChargeAmount, i);
-                        }
-                        sql.Length--; // erase last ","
-
-                        sqlRaw = sql.ToString();
-                        using (var cmd = new SqlCommand(sqlRaw, conn, trans))
-                        {
-                            affect = cmd.ExecuteNonQuery();
-
-                            if (count != 0)
-                            {
-                                var updateSql = @"UPDATE
-                                        T1
-                                    SET
-                                        [Rate] = T2.[Rate]
-                                        [MinimumChargeAmount] = T2.[MinimumChargeAmount]
-                                    FROM
-                                        RouteRate T1
-                                    JOIN
-                                        #tempTable T2 ON T2.[RecordNumber] = T1.[RecordNumber]";
-
-                                cmd.CommandText = updateSql;
-                                updateAffect = cmd.ExecuteNonQuery();
-                            }
-
-                            trans.Commit();
-                        }
-                    //} catch (Exception ex)
-                    //{
-                    //    trans.Rollback();
-                    //}
+                    sql.Append(InsertRouteRateTable());
+                } else
+                {
+                    sql.Append(CreateTempTable());
                 }
+
+                for (int i = 0; i < req.Rates.Length; i++)
+                {
+                    var rate = req.Rates[i];
+                    sql.AppendFormat("({0},{1},{2}),", rate.Rate, rate.MinimumChargeAmount, i);
+                }
+                sql.Length--; // erase last ","
+
+                sqlRaw = sql.ToString();
+
+                command.CommandText = sqlRaw;
+                affect = command.ExecuteNonQuery();
+
+                if (count != 0)
+                {
+                    command.CommandText = UpdateRouteRateTable();
+                    updateAffect = command.ExecuteNonQuery();
+                }
+
+                transaction.Commit();
             }
-            
 
             return new Ok(new { Status = 1, SqlRaw = sqlRaw, InsertAffect = affect, UpdateAffect = updateAffect });
         }
