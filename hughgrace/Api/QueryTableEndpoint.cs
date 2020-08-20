12d825c244876251c3b2b6cb83acc14e794ae501
Filipe Silva <filipe.csilvaa@gmail.com>
2020-08-20 18:33:10 -0300

    added new endpoint v41

---
+++ hughgrace/Api/QueryTableEndpoint.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+using DirectScale.Disco.Extension.Api;
+using DirectScale.Disco.Extension.Services;
+using hughgrace.Models;
+
+namespace hughgrace.Api
+{
+    public class QueryTableEndpoint : IApiEndpoint
+    {
+        private readonly IDataService _dataService;
+
+        public QueryTableEndpoint(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        public ApiDefinition GetDefinition()
+        {
+            return new ApiDefinition
+            {
+                Route = "hughgraceServices/queryTable",
+                RequireAuthentication = true
+            };
+        }
+
+        public IApiResponse Post(ApiRequest request)
+        {
+            IEnumerable<RouteRate> rates;
+            using (var connection = new SqlConnection(_dataService.ClientConnectionString.ToString()))
+            {
+                connection.Open();
+
+                rates = connection.Query<RouteRate>("SELECT * FROM RouteRate");
+            }
+
+            return new Ok(new { Status = 1, Rates = rates });
+        }
+    }
+}
