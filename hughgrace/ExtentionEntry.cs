713d62cb0f596b21b387381d4981d4a89048f7a9
Filipe Silva <filipe.csilvaa@gmail.com>
2020-08-20 17:28:53 -0300

    back to scoped v40

--- hughgrace/ExtentionEntry.cs
+++ hughgrace/ExtentionEntry.cs
@@ -13,8 +13,8 @@ namespace hughgrace
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IHook<SubmitOrderHookRequest, SubmitOrderHookResponse>, SubmitOrderHook>();
-            services.AddTransient<IHook<GetShippingHookRequest, GetShippingHookResponse>, GetShippingHook>();
+            services.AddScoped<IHook<SubmitOrderHookRequest, SubmitOrderHookResponse>, SubmitOrderHook>();
+            services.AddScoped<IHook<GetShippingHookRequest, GetShippingHookResponse>, GetShippingHook>();
 
             services.AddSingleton<IApiEndpoint, CreateTableEndpoint>();
             services.AddSingleton<IApiEndpoint, PopulateTableEndpoint>();
