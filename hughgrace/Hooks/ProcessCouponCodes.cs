b6bd60529719d523f5d611f27a03f8cea927a84a
Mahaveer Singh <mahaveer@ziplingo.com>
2021-02-11 10:04:02 +0530

    Commit changes

--- hughgrace/Hooks/ProcessCouponCodes.cs
+++ hughgrace/Hooks/ProcessCouponCodes.cs
@@ -13,17 +13,21 @@ namespace hughgrace.Hooks
         public ProcessCouponCodesHookResponse
            Invoke(ProcessCouponCodesHookRequest request, Func<ProcessCouponCodesHookRequest, ProcessCouponCodesHookResponse> func)
         {
-            if(request.LineItems.Any(x=> x.SKU == "STRKIT001" || x.SKU == "STRKIT002" || x.SKU == "STRKIT003"))
+            var hookResponse = func(request);
+
+            if (request.LineItems.Any(x=> x.SKU == "STRKIT001" || x.SKU == "STRKIT002" || x.SKU == "STRKIT003"))
             {
                 List<LineItem> includedItems = request.LineItems.Where(x => x.SKU != "STRKIT001" && x.SKU != "STRKIT002" && x.SKU != "STRKIT003").ToList();
+                var totalDiscount = includedItems.Sum(x => x.ExtendedPrice)*25/100;
 
-                var totalDiscount = includedItems.Sum(x => x.ExtendedPrice)%25;
+                var usedCoupons = hookResponse.OrderCoupons.UsedCoupons.ToList();
 
-                OrderCoupons coupons = new OrderCoupons {DiscountTotal = totalDiscount };
+                OrderCoupons coupons = new OrderCoupons {DiscountTotal = totalDiscount, UsedCoupons = usedCoupons.ToArray() };
 
                 return new ProcessCouponCodesHookResponse
                 {
-                    OrderCoupons = coupons
+                    OrderCoupons = coupons,
+                    CouponAdjustedTax = hookResponse.CouponAdjustedTax
                 };
             }
 
@@ -34,4 +38,7 @@ namespace hughgrace.Hooks
         }
 
     }
+
+
+
 }
